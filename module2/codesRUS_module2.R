a93f895c906aa010970af119035d0a34512c5c7e
Amanda Napoli <amandanapoli@amandas-air-5.bc.edu>
2021-09-13 10:36:59 -0400

    cleaning up code

--- module2/codesRUS_module2.R
+++ module2/codesRUS_module2.R
@@ -1,31 +1,12 @@
-library(tidyverse) # Remember to load your libraries!
-
-#Remember to set your working directory, too!
+#load library
+library(tidyverse) 
 
 #load data
 pseed <- read_csv("pseed.fin.amps.csv")
-## Parsed with column specification:
-## cols(
-##  fish = col_character(),
-##  speed = col_double(),
-##  frame = col_double(),
-##  date = col_character(),
-##  amp = col_double(),
-##  fin = col_character(),
-##  amp.b1 = col_double()
-## )
-
 pseed.b1 <- read_csv("pseed.lengths.csv")
-## Parsed with column specification:
-## cols(
-##  fish = col_character(),
-##  b1 = col_double()
-## )
-
 speeds <- read_csv("pseed.calibration.csv")
-## Parsed with column specification:
-## cols(
-##    vol = col_double(),
-##    m.s = col_double(),
-##    cm.s = col_double()
-## )
+
+# joining relational data
+pseed2 <- pseed%>%
+  left_join(speeds, by=c("speed" = "vol"))%>%
+  print()
