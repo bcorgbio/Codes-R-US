3eeee5e4a2d8a6757de0f6451ac015c6c046e457
Amanda Napoli <amandanapoli@amandas-air-5.bc.edu>
2021-09-13 10:21:06 -0400

    added comments

--- codesRUS_module2.R
+++ codesRUS_module2.R
@@ -4,5 +4,28 @@ library(tidyverse) # Remember to load your libraries!
 
 #load data
 pseed <- read_csv("pseed.fin.amps.csv")
+## Parsed with column specification:
+## cols(
+##  fish = col_character(),
+##  speed = col_double(),
+##  frame = col_double(),
+##  date = col_character(),
+##  amp = col_double(),
+##  fin = col_character(),
+##  amp.b1 = col_double()
+## )
+
 pseed.b1 <- read_csv("pseed.lengths.csv")
+## Parsed with column specification:
+## cols(
+##  fish = col_character(),
+##  b1 = col_double()
+## )
+
 speeds <- read_csv("pseed.calibration.csv")
+## Parsed with column specification:
+## cols(
+##    vol = col_double(),
+##    m.s = col_double(),
+##    cm.s = col_double()
+## )
