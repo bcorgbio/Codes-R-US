8a6e88ba8a103b99549a24a39d15caa311484879
nickgraz3 <grazianf@bc.edu>
2021-09-15 10:39:17 -0400

    Through Pivoting to Different Questions (but did not apply to amp.sum)

--- module2/codesRUS_module2.R
+++ module2/codesRUS_module2.R
@@ -52,3 +52,51 @@ fget(f1)
 pseed2%>%
   filter(date=="2019-06-17-151149", fin=="L")%>%
   ggplot(aes(x=frame, y=amp.bl))+geom_point()+geom_vline(xintercept = fget(f1)$crit.pts)
+f2 <-  features(x = exp1$frame,y=exp1$amp.bl*100)
+fget(f2)
+f.tib <- fget(f2)[2:3]%>%
+  as_tibble()%>%
+  filter(curvature<0)%>%
+  mutate(peaks=round(crit.pts,0))%>%
+  print()
+pseed2%>%
+  filter(date=="2019-06-17-151149", fin=="L")%>%
+  mutate(peak=frame %in% f.tib$peaks)%>%
+  ggplot(aes(x=frame,y=amp.bl,col=peak))+geom_point()
+pseed2%>%
+  summarize(n=length(unique(date)))
+find.peaks <- function(x,y,mult=100){ #define the functions parameter/inputs:x,y, and how much we won't to multiple y by (remember the rounding issue)
+  f <- fget(features(x = x,y=y*mult))[2:3]%>% #store results in `f` and compute the features for the x-y relationship, wrap in in fget to retrieve the important features, subset the results to take the 2nd and 3rd and  items, the critical points and curvature, then pass it to a tibble
+    as_tibble()%>% # pass in through a filter that returns curvatures <0
+    filter(curvature<0)%>% #add a column that rounds the critical point to an integer that represents the frame
+    mutate(peaks=round(crit.pts,0))
+  return(f$peaks) # return the peaks from tibble
+}
+pseed2%>%
+  filter(date%in%unique(date)[1:3])%>%
+  group_by(date,fin)%>%
+  mutate(peak=frame %in% find.peaks(frame,amp.bl))%>%
+  ggplot(aes(x=frame,y=amp.bl,alpha=peak,col=peak))+geom_point()+facet_grid(date~fin)
+pseed.max <- pseed2%>%
+  group_by(date,fin)%>%
+  mutate(peak=frame %in% find.peaks(frame,amp.bl))%>%
+  filter(peak==T) #new filter
+pseed.max%>%
+  ggplot(aes(x=bl.s,y=amp.bl))+geom_point()+geom_smooth(method="lm")
+amp.aov <-  aov(amp.bl~bl.s,pseed.max)
+summary(amp.aov)
+pseed.max %>%
+  group_by(fish, bl.s) %>%
+  summarize(mean.max=mean(amp.bl)) %>%
+  ggplot(aes(x=bl.s,y=mean.max,col=fish))+geom_point()+geom_smooth(method="lm")
+pseed2
+pseed2 <- pseed2 %>%
+  group_by(date,frame) %>%
+  mutate(amp.sum=sum(amp.bl))
+pseed2 %>%
+  filter(fin=="R")
+pseed.wide <- pseed2 %>%
+  select(-amp)%>%
+  pivot_wider(names_from = fin,values_from = amp.bl) %>%
+  mutate(amp.sum=L+R)%>%
+  print() 
