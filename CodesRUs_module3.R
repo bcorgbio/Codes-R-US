7b21bbdbcc63c9c16868674d8a65bac0efa6cf77
Amanda Napoli <amandanapoli@amandas-air-5.bc.edu>
2021-09-21 21:20:16 -0400

    currently writing notes for the summary(anole.allo)

--- CodesRUs_module3.R
+++ CodesRUs_module3.R
@@ -43,6 +43,7 @@ anole2%>%
 ## built linear model with HTotal predicted by SVL
 ## second parameter indicates what data the variables are coming from
 anole.lm <- lm(HTotal~SVL, anole2)
+
 #gives values of intercept and slope
 coef(anole.lm)
 
@@ -61,3 +62,17 @@ pred.lm <- tibble(
 
 anole2%>%
   ggplot(aes(SVL,HTotal))+geom_point()+geom_point(data=pred.lm, aes(SVL,H.pred), col="blue")
+#retrieves important information about the model
+summary(anole.lm)
+
+#nls() = non-linear least squares
+## least squares = approach in regression modeling which approximates the relationship by minimizing the sum of the squares of the residuals
+### how the model in our basic lm model is fit
+## nls = the model is fit based on a non-linear relationship 
+### an equation with the paramters of alpha and beta
+
+#fitting allometric model with nls
+## need to specify starting values for parameters in our model w list 
+## nls conducts search of model parameter values to find those that reduce the sum of squares of the residuals
+anole.allo <- nls(HTotal~a*SVL^b, start=list(b=1, a=1), data=anole2)
+summary(anole.allo)
