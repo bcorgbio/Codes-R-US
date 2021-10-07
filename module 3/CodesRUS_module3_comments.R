aeafddee27970e0dda3a0f102f0ad968a56287ee
ckenaley <cpkenaley@gmail.com>
2021-10-07 12:09:29 -0400

    proj3 comments

---
+++ module 3/CodesRUS_module3_comments.R
@@ -0,0 +1,174 @@
+###Project Code
+
+#1
+
+#load libraries
+library(tidyverse)
+library(ape)
+library(nlme)
+library(geiger)
+library(caper)
+library(phytools)
+library(viridis)
+library(MuMIn)
+
+#CPK, can't load your data! Remember, by default, the code looks to the project directory! [-1]. Changed to work.
+#load data
+#anole <- read_csv("anole.dat.csv")
+#anole.eco <- read_csv("anole.eco.csv")
+
+anole <- read_csv("./module 3/anole.dat.csv")
+anole.eco <- read_csv("./module 3/anole.eco.csv")
+
+# merging anole.eco to anole data set
+anole2 <- anole%>%
+  left_join(anole.eco)%>%
+  filter(!Ecomorph%in%c("U","CH"))%>%
+  na.omit()%>% ##omitted any rows of tibble that had missing values
+  print()
+
+#mutated collective tibble containing morphological and ecological data
+anole.log <- anole2 %>%
+  mutate_at(c("SVL","HTotal","PH","ArbPD"), log)
+
+#CPK: Still lots of stuff you don't need. Below are examples from the descrption [-1]
+
+#redefined anole.log as a tibble containing this new column
+anole.log <- anole.log %>%
+  mutate(res=residuals(anole.log.lm))
+
+
+# mutating and redefining anole.log data to include a column for phylogenetically corrected residuals
+#CPK:This paste of an example won't run bc you include a variable that isn't in this script: pgls.BM2. And you don't need it so commenting out.
+
+# anole.log <- anole.log %>%
+#   mutate(phylo.res=residuals(pgls.BM2))
+# p.eco.phylo <- anole.log %>%
+#   ggplot(aes(x=Ecomorph2, y=phylo.res))+geom_boxplot()+stat_summary(fun=mean, geom="point", size=3)
+# print(p.eco.phylo)
+
+
+#establish covariation matrix with tree & assume the characters evolved under evolutionary model
+
+#CPK: again, the path issue
+
+anole.tree <- read.tree("./module 3/anole.tre")
+plot(anole.tree, cex=0.4)
+
+
+#2
+
+#Constructing two simple linear models that assesses effect of PH and ArbPD by including them as covariates in the models
+anole.log.arbpd.lm <- lm(HTotal~SVL+ArbPD, anole.log)
+summary(anole.log.arbpd.lm)
+anova(anole.log.arbpd.lm)
+
+anole.log.ph.lm <- lm(HTotal~SVL+PH, anole.log)
+summary(anole.log.ph.lm)
+anova(anole.log.ph.lm)
+
+
+#3
+
+#Exploring how both ArbPD and PH effect the hindlimb-SVL relationship
+#Plotting the residuals of the simple linear models against covariates
+
+#computing the residuals based on global anole.log.arpd.lm and anole.log.ph.lm model
+#used mutate to establish a new res column that contains the residuals
+#redefined anole.log as a tibble containing this new column
+
+#before
+anole.log #CPK: Why this?
+
+anole.log <- anole.log %>%
+  mutate(pd.res=residuals(anole.log.arbpd.lm))
+#after adding ArbPD residuals
+anole.log #CPK: Why this?
+anole.log <- anole.log %>%
+  mutate(ph.res=residuals(anole.log.ph.lm))
+#after adding PH residuals
+anole.log
+
+
+
+#plotting the residuals against covariates
+p.pd <- anole.log %>%
+  ggplot(aes(x=Ecomorph2,y=pd.res)) + geom_boxplot() + stat_summary(fun=mean, geom="point", size=3)
+print(p.pd)
+
+p.ph <- anole.log %>%
+  ggplot(aes(x=Ecomorph2,y=ph.res)) + geom_boxplot() + stat_summary(fun=mean, geom="point", size=3)
+print(p.ph)
+
+#CPK didn't we need to plot the the HL against the residuals of the covariates (PH and PD). No mention of ecomorph. Prompt #3: Explore how both perch diameter and height effect the hindlimb-SVL relationship by plotting the residuals of your simple linear models against these covariates. This will require mutating a data tibble to include residuals from both models. Please produce two separate plots. Code to do so below. -1 pt for not including the right regression plots. simple as this.
+
+anole.log %>%
+  ggplot(aes(x=ph.res,y=HTotal)) + geom_point()+geom_smooth(method="lm")
+
+anole.log %>%
+  ggplot(aes(x=pd.res,y=HTotal)) + geom_point()+geom_smooth(method="lm")
+
+#4
+#Constructing phylogenetic least squares models of the hindlimb-SVL relationships that include the unique combinations of these two covariates
+
+#CPK: but you visualize results with ecomorph and this isn't included in models
+
+#PGLS model with the hindlimb-SVL relationship + perch height (under BM & OU)
+pgls.BM1 <- gls(HTotal~SVL+PH, correlation=corBrownian(1, phy=anole.tree, form=~Species), data=anole.log, method="ML")
+pgls.OU1 <- gls(HTotal~SVL+PH, correlation=corMartins(0, phy=anole.tree, form=~Species), data=anole.log, method="ML")
+#PGLS model with the hindlimb-SVL relationship + perch diameter (under BM & OU)
+pgls.BM2 <- gls(HTotal~SVL+ArbPD, correlation=corBrownian(1, phy=anole.tree, form=~Species), data=anole.log, method="ML")
+pgls.OU2 <- gls(HTotal~SVL+ArbPD, correlation=corMartins(0, phy=anole.tree, form=~Species), data=anole.log, method="ML")
+#PGLS model with the hindlimb-SVL relationship + perch height & diameter (under BM & OU)
+pgls.BM3 <- gls(HTotal~SVL+PH+ArbPD, correlation=corBrownian(1, phy=anole.tree, form=~Species), data=anole.log, method="ML")
+pgls.OU3 <- gls(HTotal~SVL+PH+ArbPD, correlation=corMartins(0, phy=anole.tree, form=~Species), data=anole.log, method="ML")
+
+#5
+#Assessing the fit of each model using AICc and AICw 
+#Comment whether one or both of the covariates is a significant predictor of hindlimb length in a phylogenetic context
+
+#perform AIC operations to see which models fit that data best
+anole.phylo.aic <- AICc(pgls.BM1, pgls.BM2, pgls.BM3, pgls.OU1, pgls.OU2, pgls.OU3)
+aicw(anole.phylo.aic$AICc)
+# results of the AIC indicate that the phylogenetically corrected regression model that includes the hindlimb-SVL relationship w/ perch height & diameter with traits evolving under BM is the best fit
+# lowest values = best fit
+# both covariates are significant predictors of hindlimb length in a phylogenetic context
+
+#CPK. Well done! But,you say significant, but that only applies if you performed a test. You just assessed the fit of the model. Could be a good fit, but covar could be non-significant.For that . . .
+
+anova(pgls.BM3)
+
+#6 
+
+#residuals of hindlimb-SVL relationship considering effects of covariates (BM3)
+#mutating and redefining anole.log data to include a column for residuals relating to the best fitting PGLS model
+
+anole.log <- anole.log %>%
+  mutate(PHPD.res=residuals(pgls.BM3))
+
+anole.log
+p.eco.phpd <- anole.log %>%
+  ggplot(aes(x=PH, y=PHPD.res)) + geom_boxplot() + stat_summary(fun=mean, geom="point", size=2)
+print(p.eco.phpd)
+anole.log
+p.eco.phpd <- anole.log %>%
+  ggplot(aes(x=ArbPD, y=PHPD.res)) + geom_boxplot() + stat_summary(fun=mean, geom="point", size=2)
+print(p.eco.phpd)
+
+#Plot that visualizes the differences in residual values of the covariates on the hindlimb residuals
+#visualizing the effect of covariates on hindlimb-SVL residuals of the best fitting PGLS model (BM3)
+
+##passed log-transformed data to ggplot, specifying hindlimb-SVL residuals for x-values and covariate residuals for y, then added point and line
+## added another argument which colors all the added geometries according to the column values Ecomorph2
+anole.log%>%
+  ggplot(aes(PH, PHPD.res, col=Ecomorph2))+geom_point()+geom_smooth(method="lm")
+
+anole.log%>%
+  ggplot(aes(ArbPD, PHPD.res, col=Ecomorph2))+geom_point()+geom_smooth(method="lm")
+
+#CPK. But again, the last prompt didn't ask about ecomorph, just to produce a plot of your own design that concisely visualizes the effect of your covariate(s) on the hindlimb residuals of the best fitting PGLS model. You'reassessing ecomorph, but it's not in your models! Would be ok if you did. In your plots, you regress the residuals against the covariates. Doesn' make sense. But, it was as simple as this.
+
+anole.log%>%
+  ggplot(aes(PHPD.res, HTotal))+geom_point()+geom_smooth(method="lm")
+
+#CPK. Good work. Just be careful about file paths, not to include what we dont' need and address the prompts with the appropriate actions. [7/10]
