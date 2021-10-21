2126fe8df959c1e036ae757c4a85dd426bde0eb9
Amanda Napoli <amandanapoli@amandas-air-5.bc.edu>
2021-10-21 16:35:39 -0400

    project 6 work

--- CodesRUS_module6.R
+++ CodesRUS_module6.R
@@ -1,19 +1,28 @@
 library(tidyverse)
-dat.f <- list.files(pattern = "couch|active.csv") %>% #find the files
-  print()
 
-dat.l <- list() #make an empty list
-
-for(i in dat.f){
-  met.dat<- unlist(strsplit(i,"_")) #split of the filename string
-  who <- met.dat[1] #store the first element
-  activity <- gsub(".csv","",met.dat[2]) #store second element, dropping CSV with gsub()
-  
-  dat.l[[i]]<- read_csv(i)%>%
-    mutate(who=who,activity=activity) #read, then store data with additional columns of metada in list as position unique to i
+f.l <- list()
+for(i in f){
+  f.i <- read_csv(i)
+  if(ncol(f.i)==1) coln <- 1
+  if(ncol(f.i)==2) coln <- 2
+  m <- strsplit(i,"_")%>%unlist
+  print(m)
+  sub <- m[2]
+  time <- m[3]%>%tolower()
+  mass <- as.numeric(gsub(".csv","",m[4]))
+  f.i <- f.i[,coln]
+  colnames(f.i) <- "Temp"
+  f.l[[i]] <- f.i%>%
+    mutate(N=1:n(),Temp=as.numeric(Temp),subject=sub,Time=time,mass=mass)
 }
 
-dat <- do.call(rbind,dat.l) #combine the list into table
+dat <- do.call(rbind,f.l)
 
 dat%>%
-  ggplot()+geom_boxplot(aes(x=activity,y=degC))+facet_grid(.~who) #plot to see what we get
+  group_by(subject,Time,mass)%>%
+  na.omit()%>%
+  filter(N>0.5*max(N))%>%
+  print()
+
+dat%>%
+  ggplot()+geom_boxplot(aes(x=mass,y=Temp))+facet_grid(.~Time) #plot to see what we get
