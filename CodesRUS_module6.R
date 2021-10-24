67105d0de0307e45b64aa147d8577fe9b81c1525
Amanda Napoli <amandanapoli@amandas-air-5.bc.edu>
2021-10-24 15:38:50 -0400

    might've done it

--- CodesRUS_module6.R
+++ CodesRUS_module6.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+f <- list.files("./proj6data",pattern=".csv",full.names=T)
+
 f.l <- list()
 for(i in f){
   f.i <- read_csv(i)
@@ -7,23 +9,55 @@ for(i in f){
   if(ncol(f.i)==2) coln <- 2
   m <- strsplit(i,"_")%>%unlist
   print(m)
-  sub <- m[2]
+  sub <- m[2]%>%tolower()
   time <- m[3]%>%tolower()
   mass <- as.numeric(gsub(".csv","",m[4]))
-  f.i <- f.i[,coln]
+  f.i <- f.i[,coln]#take only one column
   colnames(f.i) <- "Temp"
   f.l[[i]] <- f.i%>%
     mutate(N=1:n(),Temp=as.numeric(Temp),subject=sub,Time=time,mass=mass)%>%
     print()
 }
 
-dat <- do.call(rbind,f.l)
-
-dat%>%
-  group_by(subject,Time,mass)%>%
-  na.omit()%>%
-  filter(N>0.5*max(N))%>%
+dat <- do.call(rbind,f.l)%>%
   print()
 
-dat%>%
-  ggplot()+geom_boxplot(aes(x=mass,y=Temp))+facet_grid(.~subject) #plot to see what we get
+dat2 <- dat%>%
+  group_by(subject,Time,mass)%>%
+  na.omit()%>%
+  filter(N>0.95*max(N))%>%
+  summarize(m.temp=mean(Temp), n=n())%>%
+  pivot_wider(names_from=Time, values_from = c(m.temp,n))%>%
+  mutate(td=abs(m.temp_day-m.temp_night))%>%
+  print()
+
+dat2%>%
+  ggplot(aes(log(mass),log(td)))+geom_point()+geom_smooth(method="lm")
+
+
+#Inter-specific Scaling
+mean.mass = colMeans(dat2[2], na.rm=TRUE)%>%
+  print()
+mean.h.temp= colMeans(dat2[3], na.rm=TRUE)%>%
+  print()
+mean.l.temp = colMeans(dat2[4], na.rm=TRUE)%>%
+  print()
+
+Human.Data <- data.frame("Primata","Homo sapiens", mean.mass, mean.h.temp, mean.l.temp)%>%
+  print()
+names(Human.Data) <- c("Order", "species","mass.g", "T.high", "T.low")%>%
+  print()
+print(Human.Data)
+
+mammals <- read.csv("mammal.temp.csv")%>%
+  print()
+mammals.df <- rbind(mammals, Human.Data)
+print(mammals.df)
+
+temp.change <- mammals.df%>%
+  mutate(td=abs(T.high-T.low))
+
+mammals.dat <- temp.change%>%
+
+mammals.dat %>%
+  ggplot(aes(log(mass.g),log(td)))+geom_point()+geom_smooth(method="lm")
