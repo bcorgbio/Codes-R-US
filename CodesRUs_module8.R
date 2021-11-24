440f6aae90854b75a8bcada6988c917fe5721817
Amanda Napoli <amandanapoli@Amandas-MacBook-Air-5.local>
2021-11-23 20:46:57 -0500

    code?

--- CodesRUs_module8.R
+++ CodesRUs_module8.R
@@ -5,7 +5,7 @@ library(ggplot2)
 
 ang <- seq(45,157.5,length.out = 11)
 
-k <- list.files("./Project 8 Data", full.names = T)
+k <- list.files("./Project8Data", full.names = T)
 
 print(k)
 k.l <- list()
@@ -21,7 +21,6 @@ for(i in k){
 }
 
 print(k.l)
-
 #generate normalized force
 
 force.data<-do.call(rbind,k.l)
@@ -30,7 +29,7 @@ force.data$Force <- as.numeric(force.data$Force)
 
 data.force.max.each <- force.data%>%
   group_by(experiment,angle)%>%
-  dplyr::summarise(maxf=max(abs(Force)))
+  dplyr::summarise(maxf=max(abs(Force), na.rm=TRUE), n=n())
 
 data.force.max.tot <- data.force.max.each%>%
   group_by(experiment)%>%
@@ -41,7 +40,6 @@ data.force.max.each.joined<-data.force.max.each%>%
   mutate(normF=maxf/maxf2)
 
 #find the angle with maximum force
-
 ang <- seq(45,157.5,length.out = 11)
 
 data.force.max.each.joined%>%
@@ -49,34 +47,42 @@ data.force.max.each.joined%>%
 
 data.force.max.each.joined$angle[which.max(data.force.max.each.joined$normF)] #112.5
 
-normF <- c(2.496081e-05, 9.054897e-01, 2.259461e-01, 2.027892e-01, 7.102198e-05, 
-          2.332564e-01, 9.612406e-01, 2.390605e-01, 9.764806e-01, 2.422511e-01, 
-          9.844063e-01, 2.433209e-01, 9.968981e-01, 2.447969e-01, 6.578655e-05, 
-          3.992673e-05, 1.000000e+00, 2.423130e-01, 2.162966e-01, 2.426354e-01, 
-          9.920919e-01, 2.426354e-01, 2.360261e-01, 2.386711e-01, 6.330105e-05,
-          2.386711e-01, 2.535860e-01, 2.386711e-01, 2.300790e-01, 2.349558e-01,
-          2.349558e-01, 9.569234e-01, 9.627388e-01, 7.757856e-01, 2.580260e-01,
-          2.118115e-01, 2.741498e-01, 8.959674e-01, 2.848843e-01, 9.279043e-01,
-          2.968872e-01, 9.662953e-01, 3.013313e-01, 9.818046e-01, 3.054637e-01,
-          3.348147e-05, 9.931716e-01, 3.078371e-01, 2.526909e-01, 3.072117e-01,
-          1.000000e+00, 3.072117e-01, 3.014826e-01, 3.023780e-01, 7.270829e-05,
-          3.023780e-01, 2.510978e-01, 3.023780e-01, 2.933047e-01, 2.993180e-01,
-          2.993180e-01, 2.311344e-05, 9.761718e-01)
+
+force.max.each <- force.data%>%
+  group_by(experiment,angle,subject)%>%
+  dplyr::summarise(maxf=max(abs(Force), na.rm=TRUE), n=n())
+
+force.max.tot <- force.max.each%>%
+  group_by(experiment,subject)%>%
+  dplyr::summarise(maxf2=max(maxf))
+
+force.max.each.joined <- force.max.each%>%
+  left_join(force.max.tot)%>%
+  mutate(normF=maxf/maxf2)
+
+print(force.max.each.joined)
+
+dfj.control <- force.max.each.joined[which(force.max.each.joined$experiment == "control"),]
+dfj.fatigue <- force.max.each.joined[which(force.max.each.joined$experiment == "fatigue"),]
+print(dfj.fatigue)
 
 #polynomial model(not finished)
-poly.m2 <- lm(normF~poly(angle,2)) #second order
-poly.m3 <- lm(normF~poly(angle,3)) #third order
-poly.m4 <- lm(normF~poly(angle,4)) #fourth order
+poly.m2.contr <- lm(normF~poly(angle,2), dfj.control) #second order
+poly.m3.contr <- lm(normF~poly(angle,3), dfj.control) #third order
+poly.m4.contr <- lm(normF~poly(angle,4), dfj.control) #fourth order
 
-AICc(poly.m2, poly.m3, poly.m4)
+AICc(poly.m2.contr, poly.m3.contr, poly.m4.contr)
+#Second Order is lowest = best
 
-#normF.pred <- predict()
+poly.m2.fat <- lm(normF~poly(angle,2), dfj.fatigue) #second order
+poly.m3.fat <- lm(normF~poly(angle,3), dfj.fatigue) #third order
+poly.m4.fat <- lm(normF~poly(angle,4), dfj.fatigue) #fourth order
+
+AICc(poly.m2.fat, poly.m3.fat, poly.m4.fat)
+#Second Order is lowest = best
+
 x.pred <- seq(45, 157.5, length.out=1000)
 
-normF.pred <- predict(poly.m, newdata = data.force.max.each.joined(angle=x.pred))
-
-ggplot(angle, normF)+geom_point(aes(x=x.pred, y=normF.pred), col="red") + geom_point(aes(x=x.pred[which.max(normF.pred)], y=normF.pred[which.max(normF.pred)]), size=5, col="blue")
-
-x.pred[which.max(normF.pred)]
-
-normF.fat
+#normF.pred <- predict()
+normF.pred.contr <- predict(poly.m2.contr, newdata = data.frame(angle=x.pred))
+normF.pred.fat <- predict(poly.m2.fat, newdata = data.frame(angle=x.pred))
